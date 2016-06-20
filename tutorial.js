6e0bfc4b24486763467a5c0ea71a7de90fc2291d
Terrance Schubring <tschubring@gmail.com>
2016-06-19 19:49:03 -0500

    column reader about and tut

---
+++ tutorial.js
@@ -0,0 +1,22 @@
+var tutorialCode=`
+<div class="col">
+<h1>Getting Started</h1>
+<img class="landscape" src="tutorial.png" />
+<p>1. Enter contact field information. Calculations use name and birthdate fields. Tap on a field to highlight it then type information.</p>
+<img class="landscape" src="tutorial1.png" />
+<p>2. Tap any of the calculations below the contact information to read the definition in a drop down window.  Tap the window icon to toggle the window closed.</p>
+</div>
+<div class="col">
+<img class="landscape" src="tutorial3.png" />
+<p>3. Save the contact details in your Contacts by tapping Save on the right context  menu.  This menu looks like three dots. This also clears the screen. Use Search to reload the contact. </p>
+<img class="landscape" src="tutorial4.png" />
+<p>4. The Main menu is three stacked lines. Use it to search Contacts, browse the app website, browse the support website, or view this tutorial </p>
+</div>
+<div class="col">
+<img class="landscape" src="tutorial5.png" />
+<p>5. Create a Report with the right context menu by tapping Report. The app will show progress uploading and then a tap to download message. The report downloads in a browser window on your device. The Summary report has the same process. </p>
+<img class="landscape" src="tutorial6.png" />
+<p>6. Tap the search icon or select Contacts from the main menu to enter search text to find a contact.  Tap the red search icon to start the search. Tap a name in the results list to view their information. </p>
+<p>&copy;2016 Wes Hamilton</p>
+</div>
+`;
