8a00a9b65047c9fdfd9e4f616c5f9dcfc576d588
Terrance Schubring <tschubring@gmail.com>
2016-06-27 21:13:14 -0500

    order meta in report

--- metabyfieldvalue.js
+++ metabyfieldvalue.js
@@ -345,8 +345,11 @@ var metaByFieldValue={
     "30":"Three: Happy Outgoing, Optimistic. Has high energy and infectious enthusiasm. Outspoken, psychic, serious, intense, flirtatious. Motivates and persuades others. Knack for just the right word or fact to win an argument. Does well as a teacher, speaker, lawyer, actor or musician.",
     "31":"Four: Builder, Manager, Traditionalist. Does well working hands-on as a sculptor or painter. Ambitious, traditional, and loves to travel and socialize. Works long hours when motivated.  Expects people to be interested in stories about self and plans.  Likes to live with someone and pays close attention to them."
     },
-  "firstLabel":{"label":"First Age",
+  "firstLabel":{"label":"Age of Identity",
     "":"First: In your first age, you are creating your identity."
+    },
+  "firstAgeLabel":{"label":"First Age Label",
+    "":"First Age lbl: In your first age, you are creating your label."
     },
   "firstPinnacle":{"label":"Pinnacles: Events and Environments",
     "0":"",
@@ -380,7 +383,7 @@ var metaByFieldValue={
     "9":""
     },
 
-  "secondLabel":{"label":"Second Age",
+  "secondLabel":{"label":"Age of Life Skills",
     "":"Second: In your second age, you are building your life skills."
     },
   "secondPinnacle":{"label":"Pinnacles: Events and Environments",
@@ -414,7 +417,7 @@ var metaByFieldValue={
     "8":"Eight:  You may find yourself spending lots of time at work. You are driven to accomplish and produce. You have a passion to create wealth at all costs. You may find yourself dominating your fellow workers if they can’t keep up the pace.",
     "9":""
     },
-  "thirdLabel":{"label":"Third Age",
+  "thirdLabel":{"label":"Age of Purpose & Fulfillment",
     "":"Third: In your third age, you are seeking purpose and fulfillment."
     },
   "thirdPinnacle":{"label":"Pinnacles: Events and Environments",
@@ -448,7 +451,7 @@ var metaByFieldValue={
     "8":"Eight:  You may find yourself spending lots of time at work. You are driven to accomplish and produce. You have a passion to create wealth at all costs. You may find yourself dominating your fellow workers if they can’t keep up the pace.",
     "9":""
     },
-  "fourthLabel":{"label":"Fourth Age",
+  "fourthLabel":{"label":"Age of Legacy",
     "":"Fourth: In your fourth age, you are looking for ways to give back and to leave a legacy."
     },
   "fourthPinnacle":{"label":"Pinnacles: Events and Environments",
