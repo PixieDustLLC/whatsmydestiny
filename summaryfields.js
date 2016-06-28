fe6701816ea6f05c05130d16e188219b4b8cb216
Terrance Schubring <tschubring@gmail.com>
2016-06-28 00:43:01 -0500

    order meta in report

--- summaryfields.js
+++ summaryfields.js
@@ -121,8 +121,8 @@ var summaryFields=[
 {"type":"text", "align":"right", "x":6.5, "y":26, "w":2, "content":"Month"},
 {"type":"value", "align":"right", "x":9, "y":26, "w":1, "content":"personalMonth"},
 
-{"type":"text", "align":"right", "x":0, "y":28.25, "w":12, "content":"© 2016 Wes Hamilton"},
-{"type":"text", "align":"right", "x":0, "y":29, "w":12, "content":"wes@weshamilton.com"},
+{"type":"text", "align":"left", "x":0, "y":28.25, "w":12, "content":"© 2016 Wes Hamilton"},
+{"type":"text", "align":"left", "x":0, "y":29, "w":12, "content":"wes@weshamilton.com"},
 {"type":"text", "align":"right", "x":12, "y":28.25, "w":12, "content":"whatsmydestiny.com"},
 {"type":"text", "align":"right", "x":12, "y":29, "w":12, "content":"Powered by pixiedustapps.com"}
 ];
