2a5946150edbafb5ed533dd7a17e736f4206f0a8
Terrance Schubring <tschubring@gmail.com>
2016-11-29 14:47:44 -0600

    test menu

--- rui.js
+++ rui.js
@@ -502,7 +502,7 @@ roundRect:function(ctx, x, y, width, height, radius, fill, stroke) {
 menuEvent:function(type, touches){
   dbuga('menuEvent '+type);
   if(touches.length>2){dbug('');}
-  //if(type=="touchstart"){dbug('');}
+  if(type=="touchstart"){dbug('');}
   if(((type=="touchstart")||(type=="touchmove"))&&(touches.length==1)){
     rui.touching=true;
     var x=touches[0].pageX;
